1fa4e390bf6cead4093c5d278f9ec1637532f49a
Alison Reilly <reillyamr@gmail.com>
2019-05-20 19:28:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,9 +13,20 @@ class App < Sinatra::Base
      @team_name = params["team"]["name"]
      @team_motto = params["team"]["motto"]
      @team_members = params["team"]["members"]
-     @member1_name = params["team"]["members"][0]["hero_name"]
      
-    binding.pry 
+     @member1_name = params["team"]["members"][0]["hero_name"]
+     @member1_power = params["team"]["members"][0]["power"]
+     @member1_bio = params["team"]["members"][0]["bio"]
+     
+     @member2_name = params["team"]["members"][1]["hero_name"]
+     @member2_power = params["team"]["members"][1]["power"]
+     @member3_bio = params["team"]["members"][1]["bio"]
+     
+     @member3_name = params["team"]["members"][2]["hero_name"]
+     @member3_power = params["team"]["members"][2]["power"]
+     @member3_bio = params["team"]["members"][2]["bio"]
+     
+    
       
      
        
